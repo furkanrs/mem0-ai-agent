37b941330eac3dbb66b9aa38e7b528a6ed04572f
furkan <rasheedf950@gmail.com>
2026-09-07 22:43:33 +0530

    Add Mem0 Cloud quickstart

---
+++ 01-mem0-cloud-quickstart.py
@@ -0,0 +1,45 @@
+from mem0 import MemoryClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+
+# --------------------------------------------------------------
+# Initialize Mem0 client (Cloud)
+# --------------------------------------------------------------
+
+client = MemoryClient(api_key=os.getenv("MEM0_API_KEY"))
+
+# --------------------------------------------------------------
+# Message sequence
+# --------------------------------------------------------------
+
+messages = [
+    {
+        "role": "user",
+        "content": "Hi, I'm furkan. I like to build AI automations!.",
+    },
+    {
+        "role": "assistant",
+        "content": "Hello furkan! I've noted that you like to build AI automations!. I'll keep this in mind for any AI automation related recommendations or discussions.",
+    },
+]
+
+client.add(messages, user_id="default_user")
+
+# --------------------------------------------------------------
+# Search for related memories
+# --------------------------------------------------------------
+
+query = "What shall we build today?"
+
+# --------------------------------------------------------------
+# Search for related memories
+# --------------------------------------------------------------
+
+response =client.search(
+    query,
+    filters={
+        "user_id": "default_user"
+    }
+)
